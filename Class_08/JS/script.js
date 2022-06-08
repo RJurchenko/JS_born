1dcfa9aec59ad9c75cf45a004002c47107a64675
Rodions Jurčenko <104782538+RJurchenko@users.noreply.github.com>
2022-06-08 20:05:56 +0300

    a

---
+++ Class_08/JS/script.js
@@ -0,0 +1,29 @@
+// a = 45;
+
+// a=a+a^2;
+
+// b=a/2;
+
+// c=b++;
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(a*2);
+// console.log(a, b, c);
+var a = 999;
+console.log ("a vērtība (no 'galvenā zara'): "+a);
+
+{
+console.log ("a vērtība (no 'scope' - koda bloks, nodalāms ar šādām iekāvām - {}): "+a);
+}
+
+let b = 555;
+console.log ("b vērtība (no 'galvenā zara'): "+b);
+
+
+
+
+let carName = "Volvo";
+document.getElementById("demo").innerHTML = carName;

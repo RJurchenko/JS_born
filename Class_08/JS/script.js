3ec1defcbbb145e6338c8f5f47e20800b3a985af
Rodions Jurčenko <104782538+RJurchenko@users.noreply.github.com>
2022-06-08 23:08:30 +0300

    A

--- Class_08/JS/script.js
+++ Class_08/JS/script.js
@@ -1,17 +1,18 @@
-// a = 45;
+//Uzrakstīt kodu ar diviem (trīm) mainīgajiem - 
+//decimāla skaitļa pārveidošana par bināro secību.
+//Papildu uzdevums - katru bināras secības simbolu attēlot tabulas atsevišķā šūnā.
 
-// a=a+a^2;
+var x = 60;
+document.getElementById("dec").innerHTML = x;
 
-// b=a/2;
+/*
+....................
+*/
+ y = '0b00101101';
 
-// c=b++;
+// 'scope' - skripta bloks, nodalāms ar šādām iekavam - {}
+// var atslēgas vārds ļauj nodefinēt globālo mainīgo  pieejams visā koda garumā
 
-
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log(a*2);
-// console.log(a, b, c);
 var a = 999;
 console.log ("a vērtība (no 'galvenā zara'): "+a);
 
@@ -19,11 +20,32 @@ console.log ("a vērtība (no 'galvenā zara'): "+a);
 console.log ("a vērtība (no 'scope' - koda bloks, nodalāms ar šādām iekāvām - {}): "+a);
 }
 
+//let atslēgas vārds ļauj nodefinēt lokālo mainīgo - pieejams visā blokā norobežota
+// ar {} - scope, vertība var tikt mainīta
+
 let b = 555;
 console.log ("b vērtība (no 'galvenā zara'): "+b);
+{
+    //console.log("b vērtība pirms pārdefinēšanās (no 'scope'):" +b);
+    let b = 61;
+    console.log("b vērtība pēc pārdefinēšanās (no 'scope'):" +b);
+}
+
+console.log("b vērtība (no 'galvenā zara' pēč scope):" +b);
 
 
+//const atslēgas vārds ļauj nodefinēt lokālo konstanti - pieejams visā blokā norobežota
+// ar {} - scope, vertība nevar tikt mainīta
 
+const c = 100;
+console.log ("c vērtība (no 'galvenā zara'): "+c);
+{
+ //console.log("c vērtība pirms pārdefinēšanās (no 'scope'):" +b);
+ const c = 101;
+ console.log("c vērtība pēc pārdefinēšanās (no 'scope'):" +c);
+}
+
+console.log("c vērtība (no 'galvenā zara' pēč scope):" +c);
 
 let carName = "Volvo";
 document.getElementById("demo").innerHTML = carName;

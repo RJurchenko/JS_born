c29f26a0ad52afd61779b4f741134786aa77668b
Rodions Jurčenko <104782538+RJurchenko@users.noreply.github.com>
2022-06-20 21:08:30 +0300

    a

---
+++ Class_11/script.js
@@ -0,0 +1,21 @@
+var x=[1, 3, 2];
+
+document.getElementById("sequence_before").innerHTML = "Pirms: " + x[0] + " "+ x[1] + " " + x[2];
+if (x[1]>x[0])
+    {
+    [x[0],x[1]] = [x[1],x[0]];
+    }
+if (x[2]>x[0])
+    {
+    x[0]=x[0]+x[2];
+    x[2]=x[0]-x[2];
+    x[0]=x[0]-x[2];
+    }
+if (x[2]>x[1])
+    {
+    x[1]=x[1]^x[2];
+    x[2]=x[0]^x[2];
+    x[1]=x[1]^x[2];
+    }
+
+document.getElementById("sequence_after").innerHTML = "Pirms: " + x[0] + " "+ x[1] + " " + x[2];

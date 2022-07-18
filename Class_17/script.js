d97b33541391d780b28b526b3fc8ecff63c0e9c6
Rodions Jurčenko <104782538+RJurchenko@users.noreply.github.com>
2022-07-18 18:51:53 +0300

    ABC

---
+++ Class_17/script.js
@@ -0,0 +1,62 @@
+// objekta definēšana ar literāļa palīdzību
+const car = {type:"Fiat", model:"500", color:"white"};
+// literālis var būt arī pavisam tukšs {}
+// un tad īpašības (un metodes) var pievienot pēc nepieciešamības
+car.price = 1000;
+
+// objekta īpašības (property) izgūšana
+// console.log(car.type);
+// console.log(car["type"]);
+
+// // objekts ar metodi
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+// + savs piemērs
+
+// Māra piemērs
+// const EU = {
+//   countries: ['Latvija', 'Lietuva', 'Cehija'],
+  
+//   EU_classification: function () {
+//   return this.countries == prompt("Enter country name") ? "EU" : "Not EU"
+//   }
+//   };
+
+
+  let prompt_value = prompt("Enter country name:");
+
+  function EU_country_find(value, index, array) {
+    console.log(prompt_value);
+    console.log(value);
+    return value == prompt_value;}
+  
+  const EU = {
+    countries: ["Latvija", "Lietuva", "Cehija"],
+    // prompt_value: prompt("Enter country name:"),
+  /*  EU_country_find: function (value, index, array) {
+      // console.log(this.prompt_value);
+      console.log(prompt_value);
+      console.log(value);
+      // return value == this.prompt_value;},
+      return value == prompt_value;},*/
+    EU_classification: function () {
+      // return this.countries.find(this.EU_country_find) == undefined ? "Not EU" : "EU";
+      return this.countries.find(EU_country_find) == undefined ? "Not EU" : "EU";
+    },
+  };
+  
+// const cat = {
+//   catName: "The Cat",
+//   catBreed : "Himalayan",
+//   bodyType : "Cobby",
+//   fullName : function() {
+//     return this.catName + " " + this.catBreed;
+//   }
+// };
+// console.log(cat);

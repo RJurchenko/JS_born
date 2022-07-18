20824bd3bd217f403fb712948ed075b855592a2b
Rodions Jurčenko <104782538+RJurchenko@users.noreply.github.com>
2022-07-18 22:51:45 +0300

    A

---
+++ Class_18/script.js
@@ -0,0 +1,13 @@
+let grades = [91, 66, 77, 84, 66];
+let search = 66;
+
+function searchGrade (grades,search){
+  for (var i=0; i < grades.length; i++){
+    if (grades[i] == search){
+      console.log(`The grade ${search} exists`);
+      break;
+    }
+  }
+}
+
+searchGrade(grades,search);

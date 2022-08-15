a4bfaeee8a810b7365b9fc2bb872d3031848ee38
Rodions Jurčenko <104782538+RJurchenko@users.noreply.github.com>
2022-08-15 18:18:28 +0300

    15.07

--- Class_21/script.js
+++ Class_21/script.js
@@ -1,17 +1,26 @@
+alert('Welcome to JavaScriptTutorial.net!');
+
 let jsWindow = window.open(
-  "file:///C:/Users/Chuck/Desktop/AB/JS_born/Class_17/Class_17.html",
-  "about",
+  "file:///C:/Users/Chuck/Desktop/AB/JS_born/Class_21/Class_21.html",
+  //"https://www.w3schools.com/js/js_object_prototypes.asp",
+  "test",
   "height=600,width=800"
 );
 
 setTimeout(() => {
-  window.open("file:///C:/Users/Chuck/Desktop/AB/JS_born/Class_20/Class_20.html", "about");
+  window.open("file:///C:/Users/Chuck/Desktop/AB/JS_born/Class_20/Class_20.html", "test");
+  //window.open("https://www.w3schools.com/js/js_this.asp", "test");
 }, 3000);
 
 setTimeout(() => {
-  jsWindow.resizeTo(600, 300);
+  jsWindow.resizeTo(600, 300);// Will not work for third part resources because of CORS
 }, 6000);
 
 setTimeout(() => {
     jsWindow.close();
   }, 9000);
+
+  setTimeout(() => {
+    window.open("file:///C:/Users/Chuck/Desktop/AB/JS_born/Class_18/Class_18.html", "_blank");
+  }, 12000);
+  

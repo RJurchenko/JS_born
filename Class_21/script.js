84a538cdaba43e9dd25d026cddac0aa7985b2de9
Rodions Jurčenko <104782538+RJurchenko@users.noreply.github.com>
2022-08-15 21:05:02 +0300

    15.08

--- Class_21/script.js
+++ Class_21/script.js
@@ -1,26 +1,73 @@
-alert('Welcome to JavaScriptTutorial.net!');
+window.name = "original";
 
 let jsWindow = window.open(
-  "file:///C:/Users/Chuck/Desktop/AB/JS_born/Class_21/Class_21.html",
+  "http://127.0.0.1:5500/Dialogs_AB/Class_20/",
   //"https://www.w3schools.com/js/js_object_prototypes.asp",
   "test",
   "height=600,width=800"
 );
 
+//jsWindow.window.console(jsWindow.opener);
+
 setTimeout(() => {
-  window.open("file:///C:/Users/Chuck/Desktop/AB/JS_born/Class_20/Class_20.html", "test");
+  window.open("http://127.0.0.1:5500/Dialogs_AB/Class_19/", "test");
   //window.open("https://www.w3schools.com/js/js_this.asp", "test");
 }, 3000);
 
 setTimeout(() => {
-  jsWindow.resizeTo(600, 300);// Will not work for third part resources because of CORS
+  //jsWindow.resizeTo(600, 300); // Will not work for third part resources because of CORS
+  jsWindow.resizeBy(-100, -100); // -"- (?)
+  //jsWindow.moveTo(deltaX,deltaY); // -"- (?)
+  jsWindow.moveBy(50, 50); // -"- (?)
 }, 6000);
 
+/*
 setTimeout(() => {
-    jsWindow.close();
-  }, 9000);
+  jsWindow.close();
+}, 9000);
+*/
+/*
+setTimeout(() => {
+  window.open("http://127.0.0.1:5500/Dialogs_AB/Class_18/", "_blank");
+}, 12000);
+*/
 
-  setTimeout(() => {
-    window.open("file:///C:/Users/Chuck/Desktop/AB/JS_born/Class_18/Class_18.html", "_blank");
-  }, 12000);
-  
+setTimeout(() => {
+  //window.alert("Hello World!");
+  jsWindow.alert("Hello World!");
+}, 7000);
+
+setTimeout(() => {
+  jsWindow.close();
+}, 9000);
+
+
+const urlParams = new URLSearchParams(location.search);
+
+for (const [key, value] of urlParams) {
+    console.log(`${key}:${value}`);
+}
+
+keys_ = [];
+for (const key of urlParams.keys()) {
+  console.log(key);
+  keys_.push(key);
+}
+console.log(keys_);
+
+values_ = [];
+for (const value of urlParams.values()) {
+  console.log(value);
+  values_.push(value);
+}
+console.log(values_);
+
+entries_ = [];
+for (const entry of urlParams.entries()) {
+  console.log(entry);
+  entries_.push(entry);
+}
+console.log(entries_);
+
+console.log(urlParams.has('q1'));
+console.log(urlParams.has('q3'));
